7e00d5a1461e511072a4180bf47b959a8dad04d8
Tu Dang <dtu195@gmail.com>
2022-06-06 22:19:52 +0700

    Update v1.3

--- qmk_firmware/noname/config.h
+++ qmk_firmware/noname/config.h
@@ -16,8 +16,8 @@
 #define MATRIX_COLS 10
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { D3, D2, F4, F5 }
-#define MATRIX_COL_PINS { B5, B4, E6, D7, C6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { F5, F4, D1, D0 }
+#define MATRIX_COL_PINS { F7, B1, B2, B3, B6, B5, B4, E6, D7, C6 }
 
 #define UNUSED_PINS
 
@@ -25,18 +25,18 @@
 #define DIODE_DIRECTION COL2ROW
 
 #define RGB_DI_PIN F6
-// #ifdef RGB_DI_PIN
-//   #define RGBLED_NUM 2
-//   #define RGBLIGHT_HUE_STEP 8
-//   #define RGBLIGHT_SAT_STEP 8
-//   #define RGBLIGHT_VAL_STEP 8
-//   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-//   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-//   #define RGBLIGHT_ANIMATIONS
-//   #define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
-//   #define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
-//   #define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
-// #endif
+#ifdef RGB_DI_PIN
+  #define RGBLED_NUM 10
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+  #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+  #define RGBLIGHT_ANIMATIONS
+  #define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
+  #define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
+  #define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
+#endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
